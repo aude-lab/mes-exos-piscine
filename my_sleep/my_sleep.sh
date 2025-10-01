c77286372c574bda3c92a2b70f0fe5decec7e2b2
aude.diarrassouba <aude.diarrassouba@epita.fr>
2025-10-01 12:48:07 +0200

    s

--- my_sleep/my_sleep.sh
+++ my_sleep/my_sleep.sh
@@ -3,21 +3,53 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
-seconds="$1"
+arg_seconds="$1"
 
-if [ "$seconds" -lt 0 ]; then
+if [ "$arg_seconds" -eq 0 ]; then
     exit 1
 fi
 
-if [ "$seconds" -eq 0 ]; then
-    exit 0
-fi
 
-SECONDES=0
+SECONDS=0
 while true; do
-    if [ "$SECONDES" -ge "$seconds" ]; then
+    if [ "$SECONDS" -ge "$arg_seconds" ]; then
         break
     fi
 done
 
 exit 0
+
+
+
+
+SECONDS=0
+
+# Boucle d'attente
+while [ "$SECONDS" -lt "$arg_seconds" ]; do
+    # Petite pause pour éviter de surcharger le CPU
+    # Utilisation de commande built-in quand possible
+    :
+done
+
+exit 0
+
+
+
+
+
+
+
+
+if [ $# -ne 1 ]; then
+    exit 1
+fi
+
+target_seconds="$1"
+
+SECONDS=0
+
+while true; do
+    if [ "$SECONDS" -ge "$target_seconds" ]; then
+        break
+    fi
+done
