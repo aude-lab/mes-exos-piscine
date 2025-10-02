d2ebf38e41d464d424dd33c9e8161f2f2698efb0
aude.diarrassouba <aude.diarrassouba@epita.fr>
2025-10-02 16:09:54 +0200

    s

--- simple_fnmatch/simple_fnmatch.c
+++ simple_fnmatch/simple_fnmatch.c
@@ -4,30 +4,41 @@ int simple_fnmatch(const char *pattern, const char *string)
 {
     if (*pattern == '\0' && *string == '\0')
         return 0;
-        
-    if (*pattern == '*') {
+
+    if (*pattern == '*')
+    {
         const char *p = pattern + 1;
-        while (*p == '*') p++;
-        
-        if (*p == '\0') return 0;
-        
+        while (*p == '*')
+            p++;
+
+        if (*p == '\0')
+            return 0;
+
         for (const char *s = string; *s != '\0'; s++)
             if (simple_fnmatch(p, s) == 0)
                 return 0;
         return FNM_NOMATCH;
     }
-    
-    if (*pattern == '\\' && *(pattern + 1) != '\0') {
+
+    if (*pattern == '\\')
+    {
+        if (*(pattern + 1) == '\0')
+        {
+            if (*string == '\\' && *(string + 1) == '\0')
+                return 0;
+            return FNM_NOMATCH;
+        }
+        
         if (*(pattern + 1) == *string)
             return simple_fnmatch(pattern + 2, string + 1);
         return FNM_NOMATCH;
     }
-    
+
     if (*pattern == '?' && *string != '\0')
         return simple_fnmatch(pattern + 1, string + 1);
-        
+
     if (*pattern == *string && *pattern != '\0')
         return simple_fnmatch(pattern + 1, string + 1);
-        
+
     return FNM_NOMATCH;
 }
