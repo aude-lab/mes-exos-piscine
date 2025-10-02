70b3f793b4ed64b1ea84da3c318114f6176dd075
aude.diarrassouba <aude.diarrassouba@epita.fr>
2025-10-02 13:05:08 +0200

    zd

--- freq_analysis/freq_analysis.c
+++ freq_analysis/freq_analysis.c
@@ -1,68 +1,70 @@
 #include <stdio.h>
 #include <string.h>
 
-void freq_analysis(const char text[], const char table[])
-{
-    int freq[26] = {0}; 
-    int unique_count = 0;
-    
-    for (int i = 0; text[i] != '\0'; i++)
-    {
-        if (text[i] >= 'A' && text[i] <= 'Z')
-        {
+
+struct leterfreq {
+    char letter;
+    int count;
+};
+
+
+void count_frequencies(const char text[], int freq[], int *unique_count) {
+    for (int i = 0; text[i] != '\0'; i++) {
+        if (text[i] >= 'A' && text[i] <= 'Z') {
             int index = text[i] - 'A';
-            if (freq[index] == 0)
-                unique_count++;
+            if (freq[index] == 0) (*unique_count)++;
             freq[index]++;
         }
     }
-    
-    struct LetterFreq {
-        char letter;
-        int count;
-    } letters[26];
-    
-    int letter_index = 0;
-    for (int i = 0; i < 26; i++)
-    {
-        if (freq[i] > 0)
-        {
-            letters[letter_index].letter = 'A' + i;
-            letters[letter_index].count = freq[i];
-            letter_index++;
-        }
-    }
-    
-    
-    for (int i = 0; i < unique_count - 1; i++)
-    {
-        for (int j = 0; j < unique_count - i - 1; j++)
-        {
-            if (letters[j].count < letters[j + 1].count)
-            {
-               
-                struct LetterFreq temp = letters[j];
-                letters[j] = letters[j + 1];
-                letters[j + 1] = temp;
-            }
-        }
-    }
-    
-    char mapping[26]; 
-    
-    for (int i = 0; i < unique_count; i++)
-    {
-        int cipher_index = letters[i].letter - 'A';
-        mapping[cipher_index] = table[i];
-    }
-    
-    for (int i = 0; i < 26; i++)
-    {
-        if (freq[i] > 0) 
-        {
-            char cipher_letter = 'A' + i;
-            char original_letter = mapping[i];
-            printf("%c %c\n", cipher_letter, original_letter);
-        }
-    }
 }
+
+
+int constc_letter_array(int freq[], struct leterfreq letters[]) {
+    int index = 0;
+    for (int i = 0; i < 26; i++)
+        if (freq[i] > 0) {
+            letters[index].letter = 'A' + i;
+            letters[index].count = freq[i];
+            index++;
+        }
+    return index;
+}
+
+
+void sort_letters(struct leterfreq letters[], int n) {
+    for (int i = 0; i < n - 1; i++)
+        for (int j = 0; j < n - i - 1; j++)
+            if (letters[j].count < letters[j + 1].count) {
+                struct leterfreq tmp = letters[j];
+                letters[j] = letters[j + 1];
+                letters[j + 1] = tmp;
+            }
+}
+
+
+void freq_analysis(const char text[], const char table[]) {
+    int freq[26] = {0}, unique_count = 0;
+    struct leterfreq letters[26];
+    char mapping[26];
+
+    count_frequencies(text, freq, &unique_count);
+    int letter_count = constc_letter_array(freq, letters);
+    sort_letters(letters, letter_count);
+
+    for (int i = 0; i < letter_count; i++)
+        mapping[letters[i].letter - 'A'] = table[i];
+
+    for (int i = 0; i < 26; i++)
+        if (freq[i] > 0)
+            printf("%c %c\n", 'A' + i, mapping[i]);
+}
+
+
+
+
+
+
+
+
+
+
