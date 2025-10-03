833caf2e853cb00387d3bb0271db53b905b3f21c
aude.diarrassouba <aude.diarrassouba@epita.fr>
2025-10-03 13:19:38 +0200

    ze

--- simple_fnmatch/simple_fnmatch.c
+++ simple_fnmatch/simple_fnmatch.c
@@ -22,16 +22,12 @@ int simple_fnmatch(const char *pattern, const char *string)
 
     if (*pattern == '\\')
     {
-        if (*(pattern + 1) == '\0')
-        {
-            if (*string == '\\' && *(string + 1) == '\0')
-                return 0;
-            return FNM_NOMATCH;
-        }
-        
-        if (*(pattern + 1) == *string)
-            return simple_fnmatch(pattern + 2, string + 1);
-        return FNM_NOMATCH;
+	    if (*(pattern + 1) == '\0')
+        	return FNM_NOMATCH;  
+
+    	    if (*(pattern + 1) == *string)
+        	return simple_fnmatch(pattern + 2, string + 1);
+    	    return FNM_NOMATCH;
     }
 
     if (*pattern == '?' && *string != '\0')
