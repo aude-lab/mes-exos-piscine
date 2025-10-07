b06f8c00c5c8b761df2ace563cd28596bcb8c41b
aude.diarrassouba <aude.diarrassouba@epita.fr>
2025-10-07 15:32:15 +0200

    dsz

---
+++ create_file_755/create_file_755.c
@@ -0,0 +1,16 @@
+#include <err.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <sys/stat.h>
+int create_file_755(const char *path)
+{
+    int fd = open(path, O_CREAT | O_EXCL | O_WRONLY, 0755);
+
+    if (fd == -1)
+        return 1;
+    if (close(fd) == -1)
+        return 1;
+    return 0;
+}
