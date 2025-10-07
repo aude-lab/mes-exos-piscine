849caff64ca58219b5111902c3f5e7e61173c845
aude.diarrassouba <aude.diarrassouba@epita.fr>
2025-10-07 15:59:41 +0200

    ds

---
+++ spoilers/spoilers.c
@@ -0,0 +1,71 @@
+#include <ctype.h>
+#include <err.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+int main(int argcn char *argv[])
+{
+    if (argc != 2)
+    {
+        printf(stderr, "Invalid number of arguments\n");
+        return 1;
+    }
+
+    int fd = open(argv[1], O_RDONLY);
+    if (fd == -1)
+    {
+        printf(stderr, "Could not open file\n");
+        return 1;
+    }
+    off_t sizef = lseek(fd, 0, SEEK_END);
+    if (sizef == -1)
+    {
+        printf(stderr, "error\n");
+        close(fd);
+        return 1;
+    }
+
+    char result[2] = { 0 };
+    int f = 0;
+    off_t pos = sizef - 1;
+    while (pos >= 0 && f < 2)
+    {
+        if (lseek(fd, pos, SEEK_SET) == -1)
+        {
+            printf(stderr, "error\n");
+            close(fd);
+            return 1;
+        }
+        char c;
+        if (read(fd, &c, 1) != 1)
+        {
+            printf(stderr, "error\n");
+            close(fd);
+            return 1;
+        }
+        if (!isspace(c))
+        {
+            result[1 - f] = c;
+            found++;
+        }
+
+        pos--;
+    }
+
+    if (f < 2)
+
+    {
+        printf(stderr, "File is not long enough\n");
+        close(fd);
+        return 1;
+    }
+    write(STDOUT_FILENO, result, 2);
+    write(STDOUT_FILENO, "\n", 1);
+
+    close(fd);
+    return 0;
+}
