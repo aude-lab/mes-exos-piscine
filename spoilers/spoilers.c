abb76dbc77db49f4885b325e09ac0bafd3177fb9
aude.diarrassouba <aude.diarrassouba@epita.fr>
2025-10-08 17:10:21 +0200

    ds

--- spoilers/spoilers.c
+++ spoilers/spoilers.c
@@ -1,34 +1,26 @@
 #include <ctype.h>
 #include <err.h>
-#include <errno.h>
 #include <fcntl.h>
-#include <stddef.h>
 #include <stdio.h>
-#include <sys/stat.h>
 #include <unistd.h>
 
 int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
-        fprintf(stderr, "Invalid number of arguments\n");
+        write(STDERR_FILENO, "Invalid number of arguments\n", 28);
         return 1;
     }
 
     int fd = open(argv[1], O_RDONLY);
     if (fd == -1)
     {
-        fprintf(stderr, "Could not open file\n");
+        write(STDERR_FILENO, "Could not open file\n", 20);
         return 1;
     }
     off_t sizef = lseek(fd, 0, SEEK_END);
     if (sizef == -1)
-    {
-        fprintf(stderr, "error\n");
-        close(fd);
-        return 1;
-    }
-
+        err(1, "lseek");
     char result[2] = { 0 };
     int f = 0;
     off_t pos = sizef - 1;
@@ -36,21 +28,16 @@ int main(int argc, char *argv[])
     {
         if (lseek(fd, pos, SEEK_SET) == -1)
         {
-            fprintf(stderr, "error\n");
-            close(fd);
-            return 1;
+            err(1, "lseek");
         }
         char c;
         if (read(fd, &c, 1) != 1)
         {
-            fprintf(stderr, "error\n");
-            close(fd);
-            return 1;
+            err(1, "read");
         }
         if (!isspace(c))
         {
-            result[1 - f] = c;
-            f++;
+            result[1 - f++] = c;
         }
 
         pos--;
@@ -59,7 +46,7 @@ int main(int argc, char *argv[])
     if (f < 2)
 
     {
-        fprintf(stderr, "File is not long enough\n");
+        write(STDERR_FILENO, "File is not long enough\n", 24);
         close(fd);
         return 1;
     }
