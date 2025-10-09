0096961a4c33898ac06af81862f6218c12b1f289
aude.diarrassouba <aude.diarrassouba@epita.fr>
2025-10-09 03:22:05 +0200

    fqez

--- traffic_lights/traffic_lights.c
+++ traffic_lights/traffic_lights.c
@@ -1,42 +1,40 @@
 #include "traffic_lights.h"
 
-
 void init(unsigned char *lights)
 {
-    *lights = 0; 
+    *lights = 0;
 }
 
 void turn_on(unsigned char *lights, unsigned char light_num)
 {
-    *lights |= (1 << light_num);
+    unsigned char one = *lights & ~*lights;
+    one = one | ~one;
+    *lights = *lights | (one << light_num);
 }
-
 
 void turn_off(unsigned char *lights, unsigned char light_num)
 {
-    *lights &= ~(1 << light_num);
+    unsigned char one = *lights & ~*lights;
+    one = one | ~one;
+    *lights = *lights & ~(one << light_num);
 }
-
 
 void next_step(unsigned char *lights)
 {
-    unsigned char current = *lights & 0x0F; 
-    *lights = (current << 1) | (current >> 3);
+    unsigned char one = *lights & ~*lights;
+    one = one | ~one;
+    unsigned char last_bit = *lights & (one << (one + one + one));
+    *lights = (*lights >> one) | (last_bit ? one : *lights & ~*lights);
 }
-
 
 void reverse(unsigned char *lights)
 {
     *lights ^= 0x0F;
 }
 
-
 void swap(unsigned char *lights_1, unsigned char *lights_2)
 {
-    if (*lights_1 != *lights_2)
-    {
-        *lights_1 ^= *lights_2;
-        *lights_2 ^= *lights_1;
-        *lights_1 ^= *lights_2;
-    }
+    *lights_1 ^= *lights_2;
+    *lights_2 ^= *lights_1;
+    *lights_1 ^= *lights_2;
 }
