6c922f362df632d540a0791093e44d1daf1c0181
aude.diarrassouba <aude.diarrassouba@epita.fr>
2025-10-09 15:47:58 +0200

    sfs

--- traffic_lights/traffic_lights.c
+++ traffic_lights/traffic_lights.c
@@ -1,30 +1,30 @@
 #include "traffic_lights.h"
 
+#include <stdio.h>
+
 void init(unsigned char *lights)
 {
-    *lights = 0;
+    *lights = *lights & ~(*lights);
 }
 
 void turn_on(unsigned char *lights, unsigned char light_num)
 {
-    unsigned char one = *lights & ~*lights;
-    one = one | ~one;
-    *lights = *lights | (one << light_num);
+    unsigned char one = *lights | (1 << (light_num - 1));
+    if (light_num <= 4 && light_num >=1
+    	*lights = one;
 }
 
 void turn_off(unsigned char *lights, unsigned char light_num)
 {
-    unsigned char one = *lights & ~*lights;
-    one = one | ~one;
-    *lights = *lights & ~(one << light_num);
+    if (light_num <= 4 && light_num >=1
+        *lights = *lights & ~(1 << (light_num - 1));
 }
 
 void next_step(unsigned char *lights)
 {
-    unsigned char one = *lights & ~*lights;
-    one = one | ~one;
-    unsigned char last_bit = *lights & (one << (one + one + one));
-    *lights = (*lights >> one) | (last_bit ? one : *lights & ~*lights);
+    unsigned char one = (*lights & (1 << 3)) >> 3;
+    *lights = (*lights << 1) | bit;
+    *lights &= 0x0F;
 }
 
 void reverse(unsigned char *lights)
@@ -34,7 +34,10 @@ void reverse(unsigned char *lights)
 
 void swap(unsigned char *lights_1, unsigned char *lights_2)
 {
-    *lights_1 ^= *lights_2;
-    *lights_2 ^= *lights_1;
-    *lights_1 ^= *lights_2;
+    if (lights_1 != lights_2)
+    {
+        *lights_1 ^= *lights_2;
+        *lights_2 ^= *lights_1;
+        *lights_1 ^= *lights_2;
+    }
 }
