4b8316b474c131ec0df89bd7b00df309da1e14a0
aude.diarrassouba <aude.diarrassouba@epita.fr>
2025-10-11 17:54:34 +0200

    lebon

--- evalexpr/src/evalexpr.h
+++ evalexpr/src/evalexpr.h
@@ -3,39 +3,60 @@
 
 #include <stddef.h>
 
-typedef struct Stack {
+struct stack
+{
     int *data;
     size_t size;
     size_t capacity;
-} Stack;
+};
 
-typedef enum {
+enum Token_type
+{
     TOKEN_NUMBER,
     TOKEN_OPERATOR,
     TOKEN_PAREN_LEFT,
     TOKEN_PAREN_RIGHT,
     TOKEN_END
-} TokenType;
+};
+
+struct Token
+{
+    enum Token_type type;
+    int value;
+    char op;
+};
+
 
 typedef struct {
-    TokenType type;
-    int value;    
-    char op;  
-} Token;
+    const char **ptr;
+    const char **prev_token_end;
+    struct stack *op_stack;
+    char *output;
+} ShuntingContext;
 
-Stack *stack_create(void);
-void stack_push(Stack *s, int value);
-int stack_pop(Stack *s);
-int stack_peek(const Stack *s);
-int stack_is_empty(const Stack *s);
-void stack_free(Stack *s);
 
+
+int stack_is_empty(struct stack *s);
+struct stack *stack_create(void);
+void stack_push(struct stack *s, int value);
+int stack_pop(struct stack *s);
+void stack_free(struct stack *s);
+int stack_peek(struct stack *s);
+struct Token get_next_token(const char **expr);
+int stack_is_empty(struct stack *s);
 int eval_rpn(const char *expression);
-int is_operator(char c);
-int get_operator_precedence(char op);
-int apply_operator(int a, int b, char op);
+int is_a_operator(char c);
+int get_operator_priority(char op);
+int switch_operator_case(int a, int b, char op);
+char *oneshunting_yard(const char *infix);
+int evalex_infix(const char *expression);
+int is_unary_operator(char op, const char *prev_token_end);
 
-Token get_next_token(const char **expr);
-void skip_whitespace(const char **expr);
+void process_token(struct Token *token, ShuntingContext *ctx);
+void handle_number(struct Token *token, char *output);
+void handle_operator(struct Token *token, const char **prev_token_end, struct stack *op_stack, char *output);
+void handle_right_paren(struct stack *op_stack, char *output);
+void append_operator(char *output, char op);
+int flush_remaining_ops(struct stack *op_stack, char *output);
 
-#endif
+#endif /* !EVALEXPR_H */
